e3adf5538be6fb76bf0ee1658509fe76c53e2d43
Samandar <samandar0311@gmail.com>
2022-05-31 19:17:50 +0500

    commit

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,13 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	rev := make([]uint8, 0)
+	bytes := []byte(input)
+	k := len(input) - 1
+	for i := 0; i < len(input); i++ {
+		rev = append(rev, bytes[k])
+		k--
+	}
+	output = string(rev)
 	return output
 }
